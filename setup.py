1f736d0c1f0906cc59f40a0845f29e4a0f4bb1b9
zfletch <zfletch2@gmail.com>
2018-06-13 11:34:51 +0300

    setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="beta_code",
+  version="0.0.1",
+  author="zfletch",
+  author_email="zfletch2@gmail.com",
+  description="Converts Greek beta code to Greek characters and vice versa",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/zfletch/beta-code-py",
+  packages=setuptools.find_packages(),
+  classifiers=(
+    "Programming Language :: Python",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ),
+)
