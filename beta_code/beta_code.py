697dbe9cbaec2b0ac4060811f40eb7606b84f23c
zfletch <zfletch2@gmail.com>
2018-06-13 15:58:33 +0300

    beta code to unicode

--- beta_code/beta_code.py
+++ beta_code/beta_code.py
@@ -1,5 +1,6 @@
-import unicodedata
 import json
+import re
+import unicodedata
 
 with open('vendor/beta-code-json/beta_code_to_unicode.json') as json_file:
   BETA_CODE_TO_UNICODE_MAP = json.load(json_file)
@@ -7,11 +8,38 @@ with open('vendor/beta-code-json/beta_code_to_unicode.json') as json_file:
 with open('vendor/beta-code-json/unicode_to_beta_code.json') as json_file:
   UNICODE_TO_BETA_CODE_MAP = json.load(json_file)
 
+MAX_BETA_CODE_CHARACTER_LENGTH = len(max(BETA_CODE_TO_UNICODE_MAP.keys(), key=len))
+
 def greek_to_beta_code(greek):
   chars = list(unicodedata.normalize('NFC', greek))
   mapped = list(map(lambda x: UNICODE_TO_BETA_CODE_MAP.get(x, x), chars))
 
   return u''.join(mapped)
 
-def beta_code_to_greek(string):
-  return u'χαῖρε ὦ κόσμε'
+def beta_code_to_greek(beta_code):
+  beta_code_characters = list(unicodedata.normalize('NFC', beta_code))
+  greek_characters = []
+  start = 0
+
+  while start < len(beta_code_characters):
+    current_character = beta_code_characters[start]
+    new_start = start + 1
+    max_length = min(len(beta_code_characters), start + MAX_BETA_CODE_CHARACTER_LENGTH)
+
+    last = new_start
+    while last <= max_length:
+      string_slice = u''.join(beta_code_characters[start : last])
+
+      if string_slice in BETA_CODE_TO_UNICODE_MAP:
+        current_character = BETA_CODE_TO_UNICODE_MAP[string_slice]
+        new_start = last
+
+      last += 1
+
+    greek_characters.append(current_character)
+    start = new_start
+
+  return sigma_to_end_of_word_sigma(u''.join(greek_characters))
+
+def sigma_to_end_of_word_sigma(string):
+  return re.sub(r'σ(?=[,.:;·\s]|$)', u'ς', string)
--- tests/test_beta_code.py
+++ tests/test_beta_code.py
@@ -37,25 +37,25 @@ class TestBetaCode(unittest.TestCase):
       beta_code.beta_code_to_greek(u'xai=re w)= ko/sme'),
     )
 
-    # self.assertEqual(
-    #   u'μῆνιν ἄειδε θεὰ πηληϊάδεω ἀχιλῆος',
-    #   beta_code.beta_code_to_greek(u'mh=nin a)/eide qea\\ *phlhi+a/dew *)axilh=os'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'κατέβην χθὲς εἰς πειραιᾶ μετὰ γλαύκωνος τοῦ ἀρίστωνος προσευξόμενός τε τῇ θεῷ καὶ ἅμα τὴν ἑορτὴν βουλόμενος θεάσασθαι τίνα τρόπον ποιήσουσιν ἅτε νῦν πρῶτον ἄγοντες. καλὴ μὲν οὖν μοι καὶ ἡ τῶν ἐπιχωρίων πομπὴ ἔδοξεν εἶναι, οὐ μέντοι ἧττον ἐφαίνετο πρέπειν ἣν οἱ θρᾷκες ἔπεμπον. προσευξάμενοι δὲ καὶ θεωρήσαντες ἀπῇμεν πρὸς τὸ ἄστυ. κατιδὼν οὖν πόρρωθεν ἡμᾶς οἴκαδε ὡρμημένους πολέμαρχος ὁ κεφάλου ἐκέλευσε δραμόντα τὸν παῖδα περιμεῖναί ἑ κελεῦσαι. καί μου ὄπισθεν ὁ παῖς λαβόμενος τοῦ ἱματίου, κελεύει ὑμᾶς, ἔφη, πολέμαρχος περιμεῖναι. καὶ ἐγὼ μετεστράφην τε καὶ ἠρόμην ὅπου αὐτὸς εἴη. οὗτος, ἔφη, ὄπισθεν προσέρχεται· ἀλλὰ περιμένετε. ἀλλὰ περιμενοῦμεν, ἦ δ\' ὃς ὁ γλαύκων.',
-    #   beta_code.beta_code_to_greek(u'kate/bhn xqe\\s ei)s *peiraia= meta\\ *glau/kwnos tou= *)ari/stwnos proseuco/meno/s te th=| qew=| kai\\ a(/ma th\\n e(orth\\n boulo/menos qea/sasqai ti/na tro/pon poih/sousin a(/te nu=n prw=ton a)/gontes. kalh\\ me\\n ou)=n moi kai\\ h( tw=n e)pixwri/wn pomph\\ e)/docen ei)=nai, ou) me/ntoi h(=tton e)fai/neto pre/pein h(\\n oi( *qra=|kes e)/pempon. proseuca/menoi de\\ kai\\ qewrh/santes a)ph=|men pro\\s to\\ a)/stu. katidw\\n ou)=n po/rrwqen h(ma=s oi)/kade w(rmhme/nous *pole/marxos o( *kefa/lou e)ke/leuse dramo/nta to\\n pai=da perimei=nai/ e( keleu=sai. kai/ mou o)/pisqen o( pai=s labo/menos tou= i(mati/ou, keleu/ei u(ma=s, e)/fh, *pole/marxos perimei=nai. kai\\ e)gw\\ metestra/fhn te kai\\ h)ro/mhn o(/pou au)to\\s ei)/h. ou(=tos, e)/fh, o)/pisqen prose/rxetai: a)lla\\ perime/nete. a)lla\\ perimenou=men, h)= d\' o(\\s o( *glau/kwn.'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'δδδς',
-    #   beta_code.beta_code_to_greek(u'dd*ds'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'δδδς',
-    #   beta_code.beta_code_to_greek(u'dd*ds2'),
-    # )
+    self.assertEqual(
+      u'μῆνιν ἄειδε θεὰ Πηληϊάδεω Ἀχιλῆος',
+      beta_code.beta_code_to_greek(u'mh=nin a)/eide qea\\ *phlhi+a/dew *)axilh=os'),
+    )
+
+    self.assertEqual(
+      u'κατέβην χθὲς εἰς Πειραιᾶ μετὰ Γλαύκωνος τοῦ Ἀρίστωνος προσευξόμενός τε τῇ θεῷ καὶ ἅμα τὴν ἑορτὴν βουλόμενος θεάσασθαι τίνα τρόπον ποιήσουσιν ἅτε νῦν πρῶτον ἄγοντες. καλὴ μὲν οὖν μοι καὶ ἡ τῶν ἐπιχωρίων πομπὴ ἔδοξεν εἶναι, οὐ μέντοι ἧττον ἐφαίνετο πρέπειν ἣν οἱ Θρᾷκες ἔπεμπον. προσευξάμενοι δὲ καὶ θεωρήσαντες ἀπῇμεν πρὸς τὸ ἄστυ. κατιδὼν οὖν πόρρωθεν ἡμᾶς οἴκαδε ὡρμημένους Πολέμαρχος ὁ Κεφάλου ἐκέλευσε δραμόντα τὸν παῖδα περιμεῖναί ἑ κελεῦσαι. καί μου ὄπισθεν ὁ παῖς λαβόμενος τοῦ ἱματίου, κελεύει ὑμᾶς, ἔφη, Πολέμαρχος περιμεῖναι. καὶ ἐγὼ μετεστράφην τε καὶ ἠρόμην ὅπου αὐτὸς εἴη. οὗτος, ἔφη, ὄπισθεν προσέρχεται· ἀλλὰ περιμένετε. ἀλλὰ περιμενοῦμεν, ἦ δ\' ὃς ὁ Γλαύκων.',
+      beta_code.beta_code_to_greek(u'kate/bhn xqe\\s ei)s *peiraia= meta\\ *glau/kwnos tou= *)ari/stwnos proseuco/meno/s te th=| qew=| kai\\ a(/ma th\\n e(orth\\n boulo/menos qea/sasqai ti/na tro/pon poih/sousin a(/te nu=n prw=ton a)/gontes. kalh\\ me\\n ou)=n moi kai\\ h( tw=n e)pixwri/wn pomph\\ e)/docen ei)=nai, ou) me/ntoi h(=tton e)fai/neto pre/pein h(\\n oi( *qra=|kes e)/pempon. proseuca/menoi de\\ kai\\ qewrh/santes a)ph=|men pro\\s to\\ a)/stu. katidw\\n ou)=n po/rrwqen h(ma=s oi)/kade w(rmhme/nous *pole/marxos o( *kefa/lou e)ke/leuse dramo/nta to\\n pai=da perimei=nai/ e( keleu=sai. kai/ mou o)/pisqen o( pai=s labo/menos tou= i(mati/ou, keleu/ei u(ma=s, e)/fh, *pole/marxos perimei=nai. kai\\ e)gw\\ metestra/fhn te kai\\ h)ro/mhn o(/pou au)to\\s ei)/h. ou(=tos, e)/fh, o)/pisqen prose/rxetai: a)lla\\ perime/nete. a)lla\\ perimenou=men, h)= d\' o(\\s o( *glau/kwn.'),
+    )
+
+    self.assertEqual(
+      u'δδΔς',
+      beta_code.beta_code_to_greek(u'dd*ds'),
+    )
+
+    self.assertEqual(
+      u'δδΔς',
+      beta_code.beta_code_to_greek(u'dd*ds2'),
+    )
 
 if __name__ == '__main__':
   unittest.main()
