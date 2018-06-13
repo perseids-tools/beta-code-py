2c7b64f25a01e3f5dc64efa5ecc0a2b7f0726de4
zfletch <zfletch2@gmail.com>
2018-06-13 14:50:21 +0300

    unicode to beta code

--- beta_code/beta_code.py
+++ beta_code/beta_code.py
@@ -1,5 +1,17 @@
-def greek_to_beta_code(string):
-  return u'xai=re w)= ko/sme'
+import unicodedata
+import json
+
+with open('vendor/beta-code-json/beta_code_to_unicode.json') as json_file:
+  BETA_CODE_TO_UNICODE_MAP = json.load(json_file)
+
+with open('vendor/beta-code-json/unicode_to_beta_code.json') as json_file:
+  UNICODE_TO_BETA_CODE_MAP = json.load(json_file)
+
+def greek_to_beta_code(greek):
+  chars = list(unicodedata.normalize('NFC', greek))
+  mapped = list(map(lambda x: UNICODE_TO_BETA_CODE_MAP.get(x, x), chars))
+
+  return u''.join(mapped)
 
 def beta_code_to_greek(string):
   return u'χαῖρε ὦ κόσμε'
--- tests/test_beta_code.py
+++ tests/test_beta_code.py
@@ -11,25 +11,25 @@ class TestBetaCode(unittest.TestCase):
       beta_code.greek_to_beta_code(u'χαῖρε ὦ κόσμε'),
     )
 
-    # self.assertEqual(
-    #   u'mh=nin a)/eide qea\\ *phlhi+a/dew *)axilh=os',
-    #   beta_code.greek_to_beta_code(u'μῆνιν ἄειδε θεὰ πηληϊάδεω ἀχιλῆος'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'kate/bhn xqe\\s ei)s *peiraia= meta\\ *glau/kwnos tou= *)ari/stwnos proseuco/meno/s te th=| qew=| kai\\ a(/ma th\\n e(orth\\n boulo/menos qea/sasqai ti/na tro/pon poih/sousin a(/te nu=n prw=ton a)/gontes. kalh\\ me\\n ou)=n moi kai\\ h( tw=n e)pixwri/wn pomph\\ e)/docen ei)=nai, ou) me/ntoi h(=tton e)fai/neto pre/pein h(\\n oi( *qra=|kes e)/pempon. proseuca/menoi de\\ kai\\ qewrh/santes a)ph=|men pro\\s to\\ a)/stu. katidw\\n ou)=n po/rrwqen h(ma=s oi)/kade w(rmhme/nous *pole/marxos o( *kefa/lou e)ke/leuse dramo/nta to\\n pai=da perimei=nai/ e( keleu=sai. kai/ mou o)/pisqen o( pai=s labo/menos tou= i(mati/ou, keleu/ei u(ma=s, e)/fh, *pole/marxos perimei=nai. kai\\ e)gw\\ metestra/fhn te kai\\ h)ro/mhn o(/pou au)to\\s ei)/h. ou(=tos, e)/fh, o)/pisqen prose/rxetai: a)lla\\ perime/nete. a)lla\\ perimenou=men, h)= d\' o(\\s o( *glau/kwn.',
-    #   beta_code.greek_to_beta_code(u'κατέβην χθὲς εἰς πειραιᾶ μετὰ γλαύκωνος τοῦ ἀρίστωνος προσευξόμενός τε τῇ θεῷ καὶ ἅμα τὴν ἑορτὴν βουλόμενος θεάσασθαι τίνα τρόπον ποιήσουσιν ἅτε νῦν πρῶτον ἄγοντες. καλὴ μὲν οὖν μοι καὶ ἡ τῶν ἐπιχωρίων πομπὴ ἔδοξεν εἶναι, οὐ μέντοι ἧττον ἐφαίνετο πρέπειν ἣν οἱ θρᾷκες ἔπεμπον. προσευξάμενοι δὲ καὶ θεωρήσαντες ἀπῇμεν πρὸς τὸ ἄστυ. κατιδὼν οὖν πόρρωθεν ἡμᾶς οἴκαδε ὡρμημένους πολέμαρχος ὁ κεφάλου ἐκέλευσε δραμόντα τὸν παῖδα περιμεῖναί ἑ κελεῦσαι. καί μου ὄπισθεν ὁ παῖς λαβόμενος τοῦ ἱματίου, κελεύει ὑμᾶς, ἔφη, πολέμαρχος περιμεῖναι. καὶ ἐγὼ μετεστράφην τε καὶ ἠρόμην ὅπου αὐτὸς εἴη. οὗτος, ἔφη, ὄπισθεν προσέρχεται· ἀλλὰ περιμένετε. ἀλλὰ περιμενοῦμεν, ἦ δ\' ὃς ὁ γλαύκων.'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'dd*ds',
-    #   beta_code.greek_to_beta_code(u'δδδς'),
-    # )
-    #
-    # self.assertEqual(
-    #   u'*pollh\\ me\\n e)n brotoi=si kou)k a)nw/numos qea\\ ke/klhmai *ku/pris ou)ranou= t᾿ e)/sw:',
-    #   beta_code.greek_to_beta_code(u'πολλὴ μὲν ἐν βροτοῖσι κοὐκ ἀνώνυμος θεὰ κέκλημαι κύπρις οὐρανοῦ τ᾿ ἔσω·'),
-    # )
+    self.assertEqual(
+      u'mh=nin a)/eide qea\\ *phlhi+a/dew *)axilh=os',
+      beta_code.greek_to_beta_code(u'μῆνιν ἄειδε θεὰ Πηληϊάδεω Ἀχιλῆος'),
+    )
+
+    self.assertEqual(
+      u'kate/bhn xqe\\s ei)s *peiraia= meta\\ *glau/kwnos tou= *)ari/stwnos proseuco/meno/s te th=| qew=| kai\\ a(/ma th\\n e(orth\\n boulo/menos qea/sasqai ti/na tro/pon poih/sousin a(/te nu=n prw=ton a)/gontes. kalh\\ me\\n ou)=n moi kai\\ h( tw=n e)pixwri/wn pomph\\ e)/docen ei)=nai, ou) me/ntoi h(=tton e)fai/neto pre/pein h(\\n oi( *qra=|kes e)/pempon. proseuca/menoi de\\ kai\\ qewrh/santes a)ph=|men pro\\s to\\ a)/stu. katidw\\n ou)=n po/rrwqen h(ma=s oi)/kade w(rmhme/nous *pole/marxos o( *kefa/lou e)ke/leuse dramo/nta to\\n pai=da perimei=nai/ e( keleu=sai. kai/ mou o)/pisqen o( pai=s labo/menos tou= i(mati/ou, keleu/ei u(ma=s, e)/fh, *pole/marxos perimei=nai. kai\\ e)gw\\ metestra/fhn te kai\\ h)ro/mhn o(/pou au)to\\s ei)/h. ou(=tos, e)/fh, o)/pisqen prose/rxetai: a)lla\\ perime/nete. a)lla\\ perimenou=men, h)= d\' o(\\s o( *glau/kwn.',
+      beta_code.greek_to_beta_code(u'κατέβην χθὲς εἰς Πειραιᾶ μετὰ Γλαύκωνος τοῦ Ἀρίστωνος προσευξόμενός τε τῇ θεῷ καὶ ἅμα τὴν ἑορτὴν βουλόμενος θεάσασθαι τίνα τρόπον ποιήσουσιν ἅτε νῦν πρῶτον ἄγοντες. καλὴ μὲν οὖν μοι καὶ ἡ τῶν ἐπιχωρίων πομπὴ ἔδοξεν εἶναι, οὐ μέντοι ἧττον ἐφαίνετο πρέπειν ἣν οἱ Θρᾷκες ἔπεμπον. προσευξάμενοι δὲ καὶ θεωρήσαντες ἀπῇμεν πρὸς τὸ ἄστυ. κατιδὼν οὖν πόρρωθεν ἡμᾶς οἴκαδε ὡρμημένους Πολέμαρχος ὁ Κεφάλου ἐκέλευσε δραμόντα τὸν παῖδα περιμεῖναί ἑ κελεῦσαι. καί μου ὄπισθεν ὁ παῖς λαβόμενος τοῦ ἱματίου, κελεύει ὑμᾶς, ἔφη, Πολέμαρχος περιμεῖναι. καὶ ἐγὼ μετεστράφην τε καὶ ἠρόμην ὅπου αὐτὸς εἴη. οὗτος, ἔφη, ὄπισθεν προσέρχεται· ἀλλὰ περιμένετε. ἀλλὰ περιμενοῦμεν, ἦ δ\' ὃς ὁ Γλαύκων.'),
+    )
+
+    self.assertEqual(
+      u'dd*ds',
+      beta_code.greek_to_beta_code(u'δδΔς'),
+    )
+
+    self.assertEqual(
+      u'*pollh\\ me\\n e)n brotoi=si kou)k a)nw/numos qea\\ ke/klhmai *ku/pris ou)ranou= t᾿ e)/sw:',
+      beta_code.greek_to_beta_code(u'Πολλὴ μὲν ἐν βροτοῖσι κοὐκ ἀνώνυμος θεὰ κέκλημαι Κύπρις οὐρανοῦ τ᾿ ἔσω·'),
+    )
 
   def test_beta_code_to_greek(self):
     self.assertEqual(
