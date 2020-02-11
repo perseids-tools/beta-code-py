9c69c15a9a7f431b3e024e27c43e930f2d797986
zfletch <zfletch2@gmail.com>
2020-02-11 15:15:44 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="beta_code",
-  version="0.0.4",
+  version="0.0.5",
   author="perseids",
   author_email="perseids@tufts.edu",
   description="Converts Greek beta code to Greek characters and vice versa",
